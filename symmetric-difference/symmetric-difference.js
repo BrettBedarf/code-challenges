cde258ae203d6ded9149b07b186e0f6d5fe8cdf1
brett <brettbedarf.dev@gmail.com>
2021-08-09 14:09:10 -0600

    fix: symmetric-difference didn't run
    redid problem for efficient solution

--- symmetric-difference/symmetric-difference.js
+++ symmetric-difference/symmetric-difference.js
@@ -1,52 +1,36 @@
-/*
-https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/find-the-symmetric-difference
+function sym(...args) {
+	const symDiffMap = args.reduce((countsMap, next) => {
+		// Add any elements in both to reducers
+		// Put next array into a set and check for matching elements
+		// against the accumulator
 
-Algorithms: Find the Symmetric Difference
-The mathematical term symmetric difference (△ or ⊕) of two sets is the set of elements which are in either of the two sets but not in both. For example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}.
+		// Add each to map with counts and the intersection
+		// will be any with count === 1
 
-Symmetric difference is a binary operation, which means it operates on only two elements. So to evaluate an expression involving symmetric differences among three elements (A △ B △ C), you must complete one operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
+		// Use map over obj so keys can be numbers instead of just strings.
 
-Create a function that takes two or more arrays and returns an array of their symmetric difference. The returned array must contain only unique values (no duplicates).
-*/
-"use strict"
-function sym(...args)
-{
-    //remove duplicates within each array
-    args = args.map(arr=> [...new Set(arr)]);
-    //only comparing first two arrays at a time
-    let firstArr = args[0];
-    let secondArr = args[1];
-    let remaining=[];//hold extra args
-    let symDiff = [];
+		// Make sure all values in both are unique otherwise
+		// multiple in one arr would cause count above 1 even
+		// if not in second.
+		const nextSet = new Set(next);
 
-    if (args.length>2){
-        remaining=args.slice(2);
-    }
-    //compare array values and push uniques to new first array
-    while (firstArr.length)
-    {
-        let firstNum;firstArr[0];
-        let isUnique;
-        isUnique = !secondArr.some((secondNum) =>
-                                       firstNum === secondNum)//some() returns true if it finds matching number
-        if (isUnique) symDiff.push(firstNum);
+		for (const el of nextSet) {
+			// default counts[el] to 0 before trying to add 1
+			countsMap.set(el, (countsMap.get(el) ?? 0) + 1);
+		}
 
-        //filter out of both arrays either way
-        firstArr=firstArr.filter(num=> num!==firstNum);
-        secondArr=secondArr.filter(num=>num!==firstNum);
+		// Remove any elements we saw more than once.
+		countsMap.forEach((count, el) => {
+			if (count > 1) {
+				countsMap.delete(el); // maps have fast deletes
+			}
+		});
 
-    }
-    //add remaining from second array
-    if(secondArr) symDiff=symDiff.concat(secondArr);
-    //check if more arrays
-    if (args.length > 2)
-    {
-        symDiff = [symDiff];
-        symDiff=symDiff.concat(remaining);
-        return sym(...symDiff);
-    }
+		// Now counts map only contains the sym diff
+		return countsMap;
+	}, new Map());
 
-    return symDiff;
+	return [...symDiffMap.keys()];
 }
 
-
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
